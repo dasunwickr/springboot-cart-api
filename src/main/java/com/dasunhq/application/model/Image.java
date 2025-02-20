db57658e35107cc55e7a13510cc736faeeb08d01
Dasun Wickramasooriya <acc.dasunw@gmail.com>
2025-02-20 15:21:10 +0530

    fix: Update the Image entity with the notations

--- src/main/java/com/dasunhq/application/model/Image.java
+++ src/main/java/com/dasunhq/application/model/Image.java
@@ -1,15 +1,31 @@
 package com.dasunhq.application.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.sql.Blob;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String fileName;
     private String filePath;
+
+    @Lob
     private Blob image;
     private String fileType;
     private String downloadUrl;
 
-    @Many
+    @ManyToOne
+    @JoinColumn(name="product_id")
     private Product product;
 }
