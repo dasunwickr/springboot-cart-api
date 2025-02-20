b3fcdb18cdc3260cd3fc8a0083875e02369a86ac
Dasun Wickramasooriya <acc.dasunw@gmail.com>
2025-02-20 13:33:32 +0530

    feat: Initial commit

---
+++ src/main/java/com/dasunhq/application/model/Image.java
@@ -0,0 +1,4 @@
+package com.dasunhq.application.model;
+
+public class Image {
+}
---
+++ src/test/java/com/dasunhq/application/SpringbootCartApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dasunhq.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCartApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
